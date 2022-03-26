d65358eb708642ca1452f92f8078c6d8131c2bef
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-26 22:43:14 +0100

    add commit feed

--- instagram/src/main/java/com/system/design/instagram/services/AsyncWorker.java
+++ instagram/src/main/java/com/system/design/instagram/services/AsyncWorker.java
@@ -1,2 +1,14 @@
-package com.system.design.instagram.services;public class AsyncWorker {
+package com.system.design.instagram.services;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service //moving post into feed table async
+
+public class AsyncWorker {
+
+    @Async
+    public void movingPostIntoFeed(){
+
+    }
 }
--- instagram/src/main/java/com/system/design/instagram/services/FeedService.java
+++ instagram/src/main/java/com/system/design/instagram/services/FeedService.java
@@ -3,11 +3,13 @@ package com.system.design.instagram.services;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class FeedService {
-    public List<String> feedByUserId(Long id){
+    //feed is changing by new photos
+    public List<String> feedByUserId(Long userId, Integer imageCount, Date last_photo_timestamp){
         return Collections.emptyList();
     }
 }
