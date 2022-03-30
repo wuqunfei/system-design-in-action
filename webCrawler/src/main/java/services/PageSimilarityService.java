aa6838b02fdbc5682184ddc17fa64014d012e689
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-30 11:47:46 +0200

    add similirity function to compare

---
+++ webCrawler/src/main/java/services/PageSimilarityService.java
@@ -0,0 +1,28 @@
+package services;
+
+import info.debatty.java.stringsimilarity.Levenshtein;
+import org.apache.http.HttpResponse;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Service
+public class PageSimilarityService {
+    private Levenshtein levenshtein;
+
+    public PageSimilarityService() {
+        levenshtein = new Levenshtein();
+    }
+
+    public double getDistance(String originPage, String newPage) {
+        return levenshtein.distance(originPage, newPage);
+    }
+
+    public boolean isOverTimeRequest(HttpServletResponse httpResponse){
+        String header = httpResponse.getHeader("Last-Modified");
+        Date lastModifiedDate = new Date();
+        Date now = new Date();
+        return lastModifiedDate.before(now);
+    }
+}
