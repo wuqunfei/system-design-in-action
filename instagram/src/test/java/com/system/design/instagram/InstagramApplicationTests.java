4194160f030d12abd8da06a81062678a01e44f26
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-30 11:48:58 +0200

    Add maven status

--- instagram/src/test/java/com/system/design/instagram/InstagramApplicationTests.java
+++ instagram/src/test/java/com/system/design/instagram/InstagramApplicationTests.java
@@ -1,10 +1,13 @@
 package com.system.design.instagram;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InstagramApplicationTests {
+
+    @Autowired
 
     @Test
     void contextLoads() {
