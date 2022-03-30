7b04ada9c0043a043819d0321eec73ac98004db0
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-30 08:49:17 +0200

    Add moninting notes for index

--- yelp/src/main/java/com/system/desgin/yelp/model/BusinessDocument.java
+++ yelp/src/main/java/com/system/desgin/yelp/model/BusinessDocument.java
@@ -49,3 +49,11 @@ public class BusinessDocument {
 
 
 }
+
+
+//https://www.datadoghq.com/blog/monitor-elasticsearch-performance-metrics/#search-metrics
+/***
+ * Query load: Monitoring the number of queries currently in progress can give you a rough idea of how many requests your cluster is dealing with at any particular moment in time. Consider alerting on unusual spikes or dips that may point to underlying problems. You may also want to monitor the size of the search thread pool queue, which we will explain in further detail
+ * Query latency: Though Elasticsearch does not explicitly provide this metric, monitoring tools can help you use the available metrics to calculate the average query latency by sampling the total number of queries and the total elapsed time at regular intervals. Set an alert if latency exceeds a threshold, and if it fires, look for potential resource bottlenecks, or investigate whether you need to optimize
+ * Fetch latency: The second part of the search process, the fetch phase, should typically take much less time than the query phase. If you notice this metric consistently increasing, this could indicate a problem with slow disks, enriching of documents (highlighting relevant text in search results, etc.), or requesting too many results.
+ */
