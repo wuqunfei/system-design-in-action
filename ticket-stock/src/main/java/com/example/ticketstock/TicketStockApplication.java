83e9d8c5ab8d72f3956d0ed6cfc07110157d11d9
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-31 09:14:36 +0200

    add ticket stock

---
+++ ticket-stock/src/main/java/com/example/ticketstock/TicketStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketStockApplication.class, args);
+    }
+
+}
---
+++ ticket-stock/src/test/java/com/example/ticketstock/TicketStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
