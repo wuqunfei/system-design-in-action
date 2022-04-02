c40c6cc48ac555868fe98b310727dffcb19804cf
Qunfei Wu <qunfei.wu@allianz.com>
2022-04-02 18:27:52 +0200

    Adding pom

---
+++ google-doc/src/main/java/com/example/googledoc/GoogleDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.googledoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleDocApplication.class, args);
+    }
+
+}
