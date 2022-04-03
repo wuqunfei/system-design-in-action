b5fcb3f48d807d54d0813a1092ee11ca188e7407
Qunfei Wu <qunfei.wu@allianz.com>
2022-04-03 17:21:11 +0200

    add file and id service

--- cloud-storage/src/main/java/com/example/cloudstorage/services/FileStorageService.java
+++ cloud-storage/src/main/java/com/example/cloudstorage/services/FileStorageService.java
@@ -1,7 +1,22 @@
 package com.example.cloudstorage.services;
 
+import com.example.cloudstorage.services.models.FileJournaling;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class FileStorageService {
+
+    public List<FileJournaling> getBlocksById(Long namespaceId, List<Long> journalIds) {
+        return Collections.emptyList();
+    }
+    public FileJournaling uploadFile(Long namespaceId, String relativePath, List<String> blockList){
+        FileJournaling file = new FileJournaling();
+        file.setNamespaceId(namespaceId);
+        file.setRelativePath(relativePath);
+        file.setBlockList(blockList);
+        return file;
+    }
 }
--- cloud-storage/src/main/java/com/example/cloudstorage/services/models/FileJournaling.java
+++ cloud-storage/src/main/java/com/example/cloudstorage/services/models/FileJournaling.java
@@ -1,5 +1,6 @@
 package com.example.cloudstorage.services.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
@@ -17,7 +18,40 @@ public class FileJournaling {
     private Long namespaceId;
 
     private String relativePath;
+
     private List<String> blockList; //SHA-HASH KEY-1, KEY-2, KEY-3
+
+    public Long getJournalId() {
+        return journalId;
+    }
+
+    public void setJournalId(Long journalId) {
+        this.journalId = journalId;
+    }
+
+    public Long getNamespaceId() {
+        return namespaceId;
+    }
+
+    public void setNamespaceId(Long namespaceId) {
+        this.namespaceId = namespaceId;
+    }
+
+    public String getRelativePath() {
+        return relativePath;
+    }
+
+    public void setRelativePath(String relativePath) {
+        this.relativePath = relativePath;
+    }
+
+    public List<String> getBlockList() {
+        return blockList;
+    }
+
+    public void setBlockList(List<String> blockList) {
+        this.blockList = blockList;
+    }
 }
 
 class FileJournalIdClass implements Serializable {
